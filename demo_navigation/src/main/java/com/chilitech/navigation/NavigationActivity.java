5301b231676bdf1d417af49c3804cb2aa5cb0ff7
Kael <324904997@qq.com>
2021-08-25 00:21:03 +0800

    navigation new

--- demo_navigation/src/main/java/com/chilitech/navigation/NavigationActivity.java
+++ demo_navigation/src/main/java/com/chilitech/navigation/NavigationActivity.java
@@ -1,14 +1,64 @@
 package com.chilitech.navigation;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 
 public class NavigationActivity extends AppCompatActivity {
+
+    private TextView tv1;
+    private TextView tv2;
+    private TextView tv3;
+    private TextView tv4;
+    private TextView tv5;
+
+    NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.navigation_activity_main);
+        tv1 = (TextView) findViewById(R.id.tv1);
+        tv2 = (TextView) findViewById(R.id.tv2);
+        tv3 = (TextView) findViewById(R.id.tv3);
+        tv4 = (TextView) findViewById(R.id.tv4);
+        tv5 = (TextView) findViewById(R.id.tv5);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        navController = navHostFragment.getNavController();
+
+
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                navController.navigate(R.id.action_global_contentFragment);
+//                Navigation.createNavigateOnClickListener(R.id.action_categoryDetailFragment_to_contentFragment);
+
+//                Navigation.findNavController(view).navigate(R.id.action_categoryDetailFragment_to_contentFragment);
+            }
+        });
+
+        tv5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                navController.navigate(R.id.action_global_categoryDetailFragment);
+
+
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
     }
 }
