86cbed53ebdf20597b994e552ab7c1b2e11b815a
Kael <324904997@qq.com>
2021-08-24 22:29:27 +0800

    navigation

---
+++ demo_navigation/src/main/java/com/chilitech/navigation/NavigationActivity.java
@@ -0,0 +1,14 @@
+package com.chilitech.navigation;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
